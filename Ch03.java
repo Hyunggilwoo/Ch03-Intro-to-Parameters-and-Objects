880599567fc04b880f18c68067521f2a43b1678d
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-04-30 19:10:50 -0700

    Formatting the savingsTable.

--- Ch03.java
+++ Ch03.java
@@ -18,9 +18,9 @@ public class Ch03 {
 	
 	// inputBirthday
 	
-	Scanner console = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
 	
-	inputBirthday(console);
+	inputBirthday(in);
 	}
 	// Function prints out each power of the base up to that maximum power.
 	
@@ -62,21 +62,47 @@ public class Ch03 {
 	
 	// obtain values
 	
-	System.out.println ("On what day of the month were you born?");
+	System.out.println("On what day of the month were you born?");
 	int day = console.nextInt();
 	
 	System.out.println("What is the name of the month in which you were born?");
-	String month = console.nextLine();
+	String month = console.next();
 	
 	System.out.println("During what year were you born?");
 	int year = console.nextInt();
 	
+	
+	
+	
+	
 	// Prints the birthdate in a suitable format
 	
-	System.out.println ("You were born on " + month + day + "," + year + "." + "You are mighty old!");
+	System.out.println ("You were born on " + month + " " + day + ", " + year + ". " + "You're mighty old!");
 	
 	
 
 }
+	public static double savingsTable () {
+	
+	// This prints the headings of the table.
+	
+	System.out.println("        " + "Current" + "Interest" + "  New  " + "  New  ");
+	System.out.println("        " + "Balance" + "        " + "Deposit" + "Balance");
+	
+	double c = 1000; //currentBalance
+	double r = 6.5; //rate
+	//double n = newBalance;
+	double d = 1000; //deposit //(how to make deposit in a loop)
+	int year = 25; 
+	
+	// for loop with year beginning at 1
+		public static double newBalance {
+		for ( int i = 1; i <= year; i++) {
+	
+		System.out.print( c * Math.pow((1 + r/100), i) + d);
+		}
+	
+	}
 
+}
 }
